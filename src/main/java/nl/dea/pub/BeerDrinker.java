429caf10157ddc2fd42e3834aa3eb89d3bbff402
Meron Brouwer <meron.brouwer@gmail.com>
2021-02-22 09:20:45 +0100

    [refactor] Created a better singlethreaded version

--- src/main/java/nl/dea/pub/BeerDrinker.java
+++ src/main/java/nl/dea/pub/BeerDrinker.java
@@ -1,8 +1,7 @@
 package nl.dea.pub;
 
-public class BeerDrinker implements Runnable {
+public class BeerDrinker {
 
-    private static final int VOLUME_GLAS = 200;
     private final String name;
     private final Keg keg;
 
@@ -15,22 +14,11 @@ public class BeerDrinker implements Runnable {
 
     public void startDrinking() {
         while (keg.getRemainingVolume() > 0) {
-            consumedBeer += keg.tap(VOLUME_GLAS);
+            consumedBeer += keg.tap(200);
             System.out.println(this.name + " tapped a glas.");
-            try {
-                // drink
-                Thread.sleep(20);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
 
         System.out.println(this.name + " drank " + consumedBeer + "ml.");
 
-    }
-
-    public void run() {
-        startDrinking();
     }
 }
--- src/main/java/nl/dea/pub/Party.java
+++ src/main/java/nl/dea/pub/Party.java
@@ -1,7 +1,5 @@
 package nl.dea.pub;
 
-import java.util.ArrayList;
-
 public class Party {
 
     public static void main(String[] args) {
@@ -9,16 +7,8 @@ public class Party {
         // Initialize new Grolsch keg with 50l
         var grolsch = new Keg(50000);
 
-        // Create the drinkers
-        var drinkers = new ArrayList<BeerDrinker>();
-
-        drinkers.add(new BeerDrinker("Meron", grolsch));
-        drinkers.add(new BeerDrinker("Robin", grolsch));
-        drinkers.add(new BeerDrinker("Koen", grolsch));
-        drinkers.add(new BeerDrinker("Leon", grolsch));
-        drinkers.add(new BeerDrinker("Anne", grolsch));
-
-        // Create and start the threads
-        drinkers.forEach(drinker -> new Thread(drinker).start());
+        // Create the drinker
+        var drinker = new BeerDrinker("Meron", grolsch);
+        drinker.startDrinking();
     }
 }
